90a2c6a4c7f0b814f8bca75a6db7ffe0d3598051
Aleksander Pettersen <alekspett@gmail.com>
2013-10-11 21:15:14 +0200

    n

--- Assigment2/Models/BlumBlumShubGenerator.cs
+++ Assigment2/Models/BlumBlumShubGenerator.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assigment2.Interfaces;
 
 namespace Assigment2.Models
 {
-    class BlumBlumShubGenerator
+    class BlumBlumShubGenerator :IBlumBlumShubGenerator
     {
+        public ReallyBigNumber GeneratePseudoRandomNumber(long seed)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
